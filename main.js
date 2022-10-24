9e51019adbd8f609b4ce54fe0d8c6938fb3ce5f2
Alan Pangnathip <alanpangnathip@gmail.com>
2022-10-23 23:23:49 -0400

    Major refactoring...
    - Used vector projection code from collision to its own function
    - Ray intersection is now its own function
    - Ray collision works with shorter sights

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ canvas.width = size[0] * scale;
 canvas.height = size[1] * scale;
 
 class Wall {
-    constructor(x1, y1, x2, y2, width = 5, colour = "white") {
+    constructor(x1, y1, x2, y2, colour = "gray", width = 5) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
@@ -30,11 +30,12 @@ class Wall {
 }
 
 class Player {
-    constructor(x, y, radius, fov = 45, colour = "white") {
+    constructor(x, y, radius, fov = 135, colour = "gray", rayColour = "white") {
         this.x = x;
         this.y = y;
         this.fov = fov * Math.PI / 180;
-        this.colour = colour
+        this.colour = colour;
+        this.rayColour = rayColour;
         
         this.pastRotation = 0;
         this.rotateSpeed = 4 * Math.PI / 180;
@@ -43,10 +44,10 @@ class Player {
         this.accel = 0.1;
         this.velo = 0;
         this.rays = {
-            num: 25,
-            sight: 1000,
+            num: 1000,
+            sight: 250,
             density: undefined,
-        }
+        };
 
         if (this.rays.num != 1) {
             this.rays.density = this.fov / (this.rays.num - 1);
@@ -60,7 +61,61 @@ class Player {
         ctx.fill();
     }
 
-    drawRay() {
+    project(bound) {
+        let boundVec = {
+            x: bound.x2 - bound.x1, 
+            y: bound.y2 - bound.y1,
+        }
+        let playerVec = {
+            x: bound.x1 - this.x,
+            y: bound.y1 - this.y,
+        }
+        
+        let projScale = (playerVec.x * boundVec.x + playerVec.y * boundVec.y) / 
+        (boundVec.x**2 + boundVec.y**2)
+
+        let projPoint = {
+            x: bound.x1 - projScale * boundVec.x,
+            y: bound.y1 - projScale * boundVec.y,
+        }
+
+        return projPoint
+    }
+
+    rayIntersection(rayPoint, bound) {
+        let A1 = rayPoint.y - this.y,
+                B1 = this.x - rayPoint.x,
+                C1 = A1 * this.x + B1 * this.y,
+                A2 = bound.y2 - bound.y1,
+                B2 = bound.x1 - bound.x2,
+                C2 = A2 * bound.x1 + B2 * bound.y1,
+                deno = A1 * B2 - A2 * B1;
+
+        if (deno == 0) return null;
+
+        let intPoint = {
+            x: (B2 * C1 - B1 * C2) / deno,
+            y: (A1 * C2 - A2 * C1) / deno,
+        }
+
+        let intWallRatio = (intPoint.x - bound.x2) / B2,
+            intRayRatio = (intPoint.x - rayPoint.x) / B1,
+            playerToInt = Math.sqrt((intPoint.x - this.x)**2 + (intPoint.y - this.y)**2)
+
+        if (isNaN(intRayRatio)) intRayRatio = 0;
+
+        let withinWall = intWallRatio < 1 && intWallRatio > 0 && intRayRatio < 1,
+            shorterThanSight = playerToInt <= this.rays.sight
+
+        if (!withinWall || !shorterThanSight) return null;
+
+        rayPoint.x = intPoint.x;
+        rayPoint.y = intPoint.y;
+        return intPoint;
+    }
+
+
+    drawRay(bounds) {
         for (let i = 0; i < this.rays.num; i++) {
             let rayPoint,
                 rayAngle;
@@ -76,99 +131,34 @@ class Player {
                 y: this.y + this.rays.sight * Math.sin(rayAngle),
             }
 
-            walls.forEach((wall) => {
-                let A1 = rayPoint.y - this.y,
-                    B1 = this.x - rayPoint.x,
-                    C1 = A1 * this.x + B1 * this.y,
-                    A2 = wall.y2 - wall.y1,
-                    B2 = wall.x1 - wall.x2,
-                    C2 = A2 * wall.x1 + B2 * wall.y1,
-                    deno = A1 * B2 - A2 * B1;
+            bounds.sort((a, b) => {
+                let aProj = this.project(a);
+                let bProj = this.project(b);
 
-                if (deno == 0) return null;
-
-                let intPoint = {
-                    x: (B2 * C1 - B1 * C2) / deno,
-                    y: (A1 * C2 - A2 * C1) / deno,
-                }
-
-                let intWallRatio = (intPoint.x - wall.x2) / (wall.x1 - wall.x2),
-                    intRayRatio = (intPoint.x - rayPoint.x) / (this.x - rayPoint.x);
-
-                let withinWall = intWallRatio < 1 && intWallRatio > 0 && intRayRatio < 1;
-
+                let aToPlayer = Math.sqrt((aProj.x - this.x)**2 + (aProj.y - this.y)**2);
+                let bToPlayer = Math.sqrt((bProj.x - this.x)**2 + (bProj.y - this.y)**2);
                 
-                if (withinWall) {
-                    ctx.beginPath();
-                    ctx.arc(intPoint.x, intPoint.y, 5, 0, 2 * Math.PI);
-                    ctx.fill();
-                }
-            })
+                return bToPlayer - aToPlayer;
+            });
 
+            bounds.forEach((bound) => {
+                this.rayIntersection(rayPoint, bound);
+                // if (!rayInt) return
+                // ctx.beginPath();
+                // ctx.arc(rayInt.x, rayInt.y, 5, 0, 2 * Math.PI);
+                // ctx.fill();
+            });
 
             ctx.beginPath();
             ctx.moveTo(this.x, this.y);
             ctx.lineTo(rayPoint.x, rayPoint.y);
-            ctx.strokeStyle = this.colour;
+            ctx.strokeStyle = this.rayColour;
             ctx.lineWidth = 1;
             ctx.stroke();
         }
     }
 
-    isColliding(bounds) {
-        let poc,
-            hit = false;
-
-        bounds.forEach((bound) => {
-            let boundVec = {
-                x: bound.x2 - bound.x1, 
-                y: bound.y2 - bound.y1,
-            }
-            let playerVec = {
-                x: bound.x1 - this.x,
-                y: bound.y1 - this.y,
-            }
-            
-            let projScale = (playerVec.x * boundVec.x + playerVec.y * boundVec.y) / 
-            (boundVec.x**2 + boundVec.y**2)
-
-            let projuv = {
-                x: bound.x1 - projScale * boundVec.x,
-                y: bound.y1 - projScale * boundVec.y,
-            }
-
-            let distFromBound = Math.sqrt((projuv.x - this.x)**2 + (projuv.y - this.y)**2),
-                distProj = Math.sqrt((projuv.x - bound.x1)**2 + (projuv.y - bound.y1)**2),
-                distBound = Math.sqrt(boundVec.x**2 + boundVec.y**2)
-            
-            if (distProj < distBound && projScale < 0) {
-                ctx.beginPath();
-                ctx.moveTo(projuv.x, projuv.y);
-                ctx.lineTo(this.x, this.y)
-                ctx.strokeStyle = "red"
-                ctx.stroke();
-            }
-            
-            if (distFromBound < this.radius && distProj < distBound && projScale < 0) {
-                hit = true;
-                poc = {x: projuv.x, y: projuv.y, wallWidth: 5}
-            } 
-        });
-
-        if (hit) {
-            return poc
-        }   else return false;
-    }
-    
-    movement(bounds) {
-        // let colliding = this.isColliding(bounds);
-        // if (colliding) {
-        //     let pastRotation = this.rotation
-        //     this.x = colliding.x + this.radius * Math.sin(-this.rotation);
-        //     this.y = colliding.y + this.radius * Math.cos(-this.rotation);
-        //     this.velo = 0;
-        // }
-
+    movement() {
         if (!keys.up.pressed && !keys.down.pressed) {
             if (Math.abs(this.velo) < 0.001) this.velo = 0;
             this.velo *= 0.9
@@ -206,8 +196,8 @@ class Player {
 let player = new Player(canvas.width / 2, canvas.height / 2, 15);
 
 let walls = [
-    new Wall(300, 300, 100, 300), 
-    new Wall(100, 200, 200, 50)
+    new Wall(100, 200, 200, 50),
+    new Wall(150, 100, 100, 300), 
 ]
 
 let keys = {
@@ -262,14 +252,13 @@ window.addEventListener('mousemove', (e) => {
     mouse.y = Math.round((e.clientY - bounds.top) * scale);
   });
 
-  
   function main() {
-      requestAnimationFrame(main);
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      player.drawRay();
-      walls.forEach((wall) => wall.draw());
-      player.draw();
-      player.movement(walls);
+    requestAnimationFrame(main);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.drawRay(walls);
+    walls.forEach((wall) => wall.draw());
+    player.draw();
+    player.movement(walls);
 }
 
 main();
