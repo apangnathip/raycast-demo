462f502d21e54282eee05e8e338c1db5abe2e678
Alan Pangnathip <alanpangnathip@gmail.com>
2022-10-29 11:13:44 -0400

    Changed line intersection method
    Now uses vector cross products instead of the standard form of a line.

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-var canvas = document.querySelector("canvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 const scale = window.devicePixelRatio;
 ctx.scale(scale, scale);
 
-var size = [600, 300];
+const size = [600, 300];
 canvas.style.width = size[0] + "px";
 canvas.style.height = size[1] + "px";
 canvas.width = size[0] * scale;
 canvas.height = size[1] * scale;
 
-class Wall {
-    constructor(x1, y1, x2, y2, colour = "gray", width = 5) {
+class Bound {
+    /**
+     * Objects that can block rays, in other words, walls.
+     * @param {float} x1 
+     * @param {float} y1 
+     * @param {float} x2 
+     * @param {float} y2 
+     * @param {string} colour 
+     * @param {float} width 
+     */
+    constructor(x1, y1, x2, y2, colour = "white", width = 2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
@@ -30,26 +39,41 @@ class Wall {
 }
 
 class Player {
-    constructor(x, y, radius, fov = 135, colour = "gray", rayColour = "white") {
+    /**
+     * User controlled object.
+     * @param {float} x
+     * @param {float} y
+     * @param {float} radius
+     * @param {float} fov - field of view displayed by rays
+     * @param {string} colour
+     * @param {string} rayColour
+     * @property {float} rotation
+     * @property {float} pastRotation - rotation before user input is removed
+     * @property {float} rotateSpeed
+     * @property {float} accel - acceleration rate
+     * @property {float} velo
+     * @property {object} rays - property of rays
+     */
+    constructor(x, y, radius, fov = 135, colour = "white", rayColour = "white") {
         this.x = x;
         this.y = y;
+        this.radius = radius;
         this.fov = fov * Math.PI / 180;
         this.colour = colour;
         this.rayColour = rayColour;
         
+        this.rotation = 0;
         this.pastRotation = 0;
         this.rotateSpeed = 4 * Math.PI / 180;
-        this.rotation = 0;
-        this.radius = radius;
         this.accel = 0.1;
         this.velo = 0;
         this.rays = {
-            num: 1000,
-            sight: 250,
+            num: 50,
+            sight: 500,
             density: undefined,
         };
 
-        if (this.rays.num != 1) {
+        if (this.rays.num != 1 && this.rays.num != 360) {
             this.rays.density = this.fov / (this.rays.num - 1);
         } else this.rays.density = this.fov
     }
@@ -61,103 +85,76 @@ class Player {
         ctx.fill();
     }
 
-    project(bound) {
-        let boundVec = {
-            x: bound.x2 - bound.x1, 
-            y: bound.y2 - bound.y1,
-        }
-        let playerVec = {
-            x: bound.x1 - this.x,
-            y: bound.y1 - this.y,
-        }
-        
-        let projScale = (playerVec.x * boundVec.x + playerVec.y * boundVec.y) / 
-        (boundVec.x**2 + boundVec.y**2)
-
-        let projPoint = {
-            x: bound.x1 - projScale * boundVec.x,
-            y: bound.y1 - projScale * boundVec.y,
-        }
-
-        return projPoint
-    }
-
-    rayIntersection(rayPoint, bound) {
-        let A1 = rayPoint.y - this.y,
-                B1 = this.x - rayPoint.x,
-                C1 = A1 * this.x + B1 * this.y,
-                A2 = bound.y2 - bound.y1,
-                B2 = bound.x1 - bound.x2,
-                C2 = A2 * bound.x1 + B2 * bound.y1,
-                deno = A1 * B2 - A2 * B1;
-
-        if (deno == 0) return null;
-
-        let intPoint = {
-            x: (B2 * C1 - B1 * C2) / deno,
-            y: (A1 * C2 - A2 * C1) / deno,
-        }
-
-        let intWallRatio = (intPoint.x - bound.x2) / B2,
-            intRayRatio = (intPoint.x - rayPoint.x) / B1,
-            playerToInt = Math.sqrt((intPoint.x - this.x)**2 + (intPoint.y - this.y)**2)
-
-        if (isNaN(intRayRatio)) intRayRatio = 0;
-
-        let withinWall = intWallRatio < 1 && intWallRatio > 0 && intRayRatio < 1,
-            shorterThanSight = playerToInt <= this.rays.sight
-
-        if (!withinWall || !shorterThanSight) return null;
-
-        rayPoint.x = intPoint.x;
-        rayPoint.y = intPoint.y;
-        return intPoint;
-    }
-
-
+    /**
+     * Draw each ray sequentially.
+     * @param {list} bounds - Objects which can block rays
+     */
     drawRay(bounds) {
         for (let i = 0; i < this.rays.num; i++) {
-            let rayPoint,
-                rayAngle;
+            let ray = {
+                point: undefined,
+                angle: undefined,
+            };
                 
+            // Offset all rays so that it aligns with player rotation.
             if (this.rays.num != 1) {
-                rayAngle = (this.rotation + i * this.rays.density) + Math.PI/2 - this.fov / 2;
+                ray.angle = (this.rotation + i * this.rays.density) + (Math.PI / 2 - this.fov / 2);
             } else {
-                rayAngle =  this.rotation + Math.PI / 2;
+                ray.angle =  this.rotation + Math.PI / 2;
             }
 
-            rayPoint = {
-                x: this.x + this.rays.sight * Math.cos(rayAngle), 
-                y: this.y + this.rays.sight * Math.sin(rayAngle),
+            ray.point = {
+                x: this.x + this.rays.sight * Math.cos(ray.angle), 
+                y: this.y + this.rays.sight * Math.sin(ray.angle),
             }
-
+            
+            // Sort bounds by whichever one the current ray hits first.
             bounds.sort((a, b) => {
-                let aProj = this.project(a);
-                let bProj = this.project(b);
-
-                let aToPlayer = Math.sqrt((aProj.x - this.x)**2 + (aProj.y - this.y)**2);
-                let bToPlayer = Math.sqrt((bProj.x - this.x)**2 + (bProj.y - this.y)**2);
+                let aPoint = this.rayIntersection(ray, a),
+                    bPoint = this.rayIntersection(ray, b);
                 
-                return bToPlayer - aToPlayer;
-            });
+                if (!aPoint) return 1;
+                if (!bPoint) return -1;
 
-            bounds.forEach((bound) => {
-                this.rayIntersection(rayPoint, bound);
-                // if (!rayInt) return
-                // ctx.beginPath();
-                // ctx.arc(rayInt.x, rayInt.y, 5, 0, 2 * Math.PI);
-                // ctx.fill();
-            });
+                let playerToA = Math.hypot(this.x - aPoint.x, this.y - aPoint.y);
+                let playerToB = Math.hypot(this.x - bPoint.x, this.y - bPoint.y);
+                
+                return playerToA - playerToB;
+            })
+
+            let point = this.rayIntersection(ray, bounds[0]);
+            if (point) ray.point = point
 
             ctx.beginPath();
             ctx.moveTo(this.x, this.y);
-            ctx.lineTo(rayPoint.x, rayPoint.y);
+            ctx.lineTo(ray.point.x, ray.point.y);
             ctx.strokeStyle = this.rayColour;
             ctx.lineWidth = 1;
             ctx.stroke();
         }
     }
 
+
+    /**
+     * Calculates point of intersection between a ray and a bound.
+     * @param {Object} ray - A line emitted from player
+     * @param {Bound} bound - Objects which can block rays
+     * @returns {Object} point of intersection
+     */
+    rayIntersection(ray, bound) {
+        let r = {x: ray.point.x - this.x, y: ray.point.y - this.y};
+        let s = {x: bound.x2 - bound.x1, y: bound.y2 - bound.y1};
+        
+        let deno = r.x * s.y - r.y * s.x;
+        let u = ((bound.x1 - this.x) * r.y - (bound.y1 - this.y) * r.x) / deno;
+        let t = ((bound.x1 - this.x) * s.y - (bound.y1 - this.y) * s.x) / deno;
+
+        return (u >= 0 && u <= 1 && t >= 0 && t <= 1) && {x: this.x + r.x*t, y: this.y + r.y*t};
+    }
+
+    /**
+     * Handles arrowkey movements
+     */
     movement() {
         if (!keys.up.pressed && !keys.down.pressed) {
             if (Math.abs(this.velo) < 0.001) this.velo = 0;
@@ -193,24 +190,22 @@ class Player {
     }
 }
 
-let player = new Player(canvas.width / 2, canvas.height / 2, 15);
+let player = new Player(canvas.width / 2, canvas.height / 2, 15),
+    walls = [
+        new Bound(100, 200, 200, 200), 
+        new Bound(150, 100, 100, 300),
+        new Bound(100, canvas.height/2, 200, canvas.height/2),
+        new Bound(canvas.width/2, 300, canvas.width/2, 500),
+    ];
 
-let walls = [
-    new Wall(100, 200, 200, 50),
-    new Wall(150, 100, 100, 300), 
-]
+let mouse = {x: 0, y: 0,},
+    keys = {
+        up: {pressed: false},
+        left: {pressed: false},
+        down: {pressed: false},
+        right: {pressed: false},
+    };
 
-let keys = {
-    up: {pressed: false},
-    left: {pressed: false},
-    down: {pressed: false},
-    right: {pressed: false},
-}
-
-let mouse = {
-    x: 0,
-    y: 0,
-}
 
 window.addEventListener("keydown", (e) => {
     switch (e.key) {
@@ -246,17 +241,12 @@ window.addEventListener("keyup", (e) => {
     }
 });
 
-window.addEventListener('mousemove', (e) => {
-    let bounds = canvas.getBoundingClientRect();
-    mouse.x = Math.round((e.clientX - bounds.left) * scale);
-    mouse.y = Math.round((e.clientY - bounds.top) * scale);
-  });
 
-  function main() {
+function main() {
     requestAnimationFrame(main);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    player.drawRay(walls);
     walls.forEach((wall) => wall.draw());
+    player.drawRay(walls);
     player.draw();
     player.movement(walls);
 }
