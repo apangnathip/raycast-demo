0e1ce2b46e08c0e806b1b91af6bb2aa38183279f
Alan Pangnathip <alanpangnathip@gmail.com>
2022-10-30 01:09:01 -0400

    Building walls.
    - implemented wall creation
    - added floor and ceiling although it looks odd when in an uncontained room

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ cCanvas.style.width = rCanvas.style.width = size[0] + "px";
 cCanvas.style.height = rCanvas.style.height = size[1] + "px";
 cCanvas.width = rCanvas.width = size[0] * scale;
 cCanvas.height = rCanvas.height = size[1] * scale;
+const boundingRect = cCanvas.getBoundingClientRect();
 
 class Bound {
     /**
@@ -26,14 +27,50 @@ class Bound {
      * @param {string} colour 
      * @param {float} width 
      */
-    constructor(x1, y1, x2, y2, width = 2, height = 100, colour = "white") {
+    constructor(x1, y1, x2, y2, colour = "255, 255, 255") {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        this.width = width;
-        this.height = height;
         this.colour = colour;
+        this.height = 100;
+        this.width = 2;
+
+        this.creating = false;
+        this.potens = {
+            x: undefined, 
+            y: undefined
+        };
+    }
+
+    static create() {
+        cctx.beginPath();
+        
+        let onCanvas = mouse.x <= cCanvas.width && mouse.y <= cCanvas.height && mouse.x >= 0 && mouse.y >= 0;
+
+        if (mouse.hold && onCanvas) {
+            if (!mouse.clicked) {
+                this.potens = {x: mouse.x, y: mouse.y};
+                this.creating = true;
+            }
+            // cctx.arc(this.potens.x, this.potens.y, 10, 0, 2* Math.PI);
+            cctx.moveTo(this.potens.x, this.potens.y)
+            mouse.clicked = true;
+
+            
+            cctx.lineTo(mouse.x, mouse.y);
+            cctx.strokeStyle = "red";
+            cctx.closePath();
+            cctx.stroke();
+
+
+        } else if (!mouse.hold && onCanvas) {
+            mouse.clicked = false;
+            if (this.creating) {
+                this.creating = false;
+                return new Bound(this.potens.x, this.potens.y, mouse.x, mouse.y);
+            }
+        }
     }
 
     draw() {
@@ -41,7 +78,7 @@ class Bound {
         cctx.moveTo(this.x1,this.y1);
         cctx.lineTo(this.x2, this.y2);
         cctx.lineWidth = this.width;
-        cctx.strokeStyle = this.colour;
+        cctx.strokeStyle = `rgba(${this.colour}, 1)`;
         cctx.stroke();
     }
 }
@@ -70,6 +107,12 @@ class Player {
         this.rotateSpeed = 4 * Math.PI / 180;
         this.accel = 0.1;
         this.velo = 0;
+        this.z = 0;
+        this.bob = 0;
+        this.bobbage = {
+            speed: 0.3,
+            ferocity: 2,
+        };
     }
 
     draw() {
@@ -86,6 +129,9 @@ class Player {
         if (!keys.up.pressed && !keys.down.pressed) {
             if (Math.abs(this.velo) < 0.001) this.velo = 0;
             this.velo *= 0.9
+            if (Math.abs(this.z) < 0.001) this.z = 0;
+            this.z *= 0.9;
+            this.bob = 0;
         }
 
         if (keys.up.pressed) {
@@ -94,6 +140,9 @@ class Player {
             }
             this.velo += this.accel;
             this.pastRotation = this.rotation;
+
+            this.z = Math.sin(this.bob) * this.bobbage.ferocity;
+            this.bob += this.bobbage.speed;
         } 
 
         if (keys.down.pressed) {
@@ -102,6 +151,9 @@ class Player {
             }
             this.velo -= this.accel;
             this.pastRotation = this.rotation;
+
+            this.z = Math.sin(this.bob) * this.bobbage.ferocity / 2;
+            this.bob += this.bobbage.speed;
         }
 
         if (keys.left.pressed) {
@@ -145,40 +197,48 @@ class Rays {
         for (let i = 0; i < this.num; i++) {
             let ray = {point: undefined, angle: undefined};
                 
-            if (this.num != 1) {
+            if (this.num == 1) {
+                ray.angle = this.emitter.rotation;
+            } else if (this.fov < Math.PI) {
                 ray.angle = this.emitter.rotation + Math.atan(this.segmentLen * i - this.fovHalfLen);
-            } else ray.angle = this.emitter.rotation;
-
+            } else {
+                ray.angle = this.emitter.rotation + (this.fov / (this.num - 1)) * i - this.fov/2;
+            }
             ray.point = {
                 x: this.emitter.x + this.sight * Math.cos(ray.angle), 
                 y: this.emitter.y + this.sight * Math.sin(ray.angle),
             }
             
             // Sort bounds by whichever one the current ray hits first.
-            bounds.sort((a, b) => {
-                let aPoint = this.rayIntersection(ray, a),
-                    bPoint = this.rayIntersection(ray, b);
-                
-                if (!aPoint) return 1;
-                if (!bPoint) return -1;
+            if (bounds.length) {
+                bounds.sort((a, b) => {
+                    let aPoint = this.rayIntersection(ray, a),
+                        bPoint = this.rayIntersection(ray, b);
+                    
+                    if (!aPoint) return 1;
+                    if (!bPoint) return -1;
+    
+                    let playerToA = Math.hypot(this.emitter.x - aPoint.x, this.emitter.y - aPoint.y),
+                        playerToB = Math.hypot(this.emitter.x - bPoint.x, this.emitter.y - bPoint.y);
+                    
+                    return playerToA - playerToB;
+                })
+    
+                let point = this.rayIntersection(ray, bounds[0]);
+                if (point) {
+                    cctx.strokeStyle = `rgba(${bounds[0].colour}, 1)`;
+                    ray.point = point
+                } else cctx.strokeStyle = this.colour;
 
-                let playerToA = Math.hypot(this.emitter.x - aPoint.x, this.emitter.y - aPoint.y),
-                    playerToB = Math.hypot(this.emitter.x - bPoint.x, this.emitter.y - bPoint.y);
-                
-                return playerToA - playerToB;
-            })
-
-            let point = this.rayIntersection(ray, bounds[0]);
-            if (point) ray.point = point
-
-            this.render(ray, i, bounds[0]);
+                this.render(ray, i, bounds[0]);
+            } else cctx.strokeStyle = this.colour;
 
             cctx.beginPath();
             cctx.moveTo(this.emitter.x, this.emitter.y);
             cctx.lineTo(ray.point.x, ray.point.y);
-            cctx.strokeStyle = this.rayColour;
             cctx.lineWidth = 1;
             cctx.stroke();
+
         }
     }
 
@@ -206,51 +266,79 @@ class Rays {
      * @param {Bound} bound 
      */
     render(ray, curr, bound) {
-        let spacing = Math.round(rCanvas.width / (this.num)),
+        let spacing = rCanvas.width / (this.num),
             dist = Math.hypot(ray.point.x - this.emitter.x, ray.point.y - this.emitter.y);
-
-        rctx.fillStyle = `rgba(255,255,255, ${1 - (dist / this.sight)})`;
-
+        
+        rctx.fillStyle = `rgba(${bound.colour}, ${1 - (dist / this.sight)})`;
+        
         let angleDiff = ray.angle - (this.emitter.rotation);
         dist *= Math.cos(angleDiff)
-      
-        let rectLength = (bound.height * rCanvas.height) / dist;
-        if (rectLength >= rCanvas.height) rectLength = rCanvas.height;
         
-        let yPos = rCanvas.height/2 - rectLength/2;
-        rctx.fillRect(spacing * (curr), yPos, spacing, rectLength);
+        // Bounds
+        let rectLen = (bound.height * rCanvas.height) / dist,        
+            boundPos = (rCanvas.height / 2 - rectLen / 2) + this.emitter.z;
+        rctx.fillRect(spacing * curr, boundPos, spacing, rectLen);
+        
+
+        // Floor
+        let floorPos = (rCanvas.height / 2 + rectLen / 2) + this.emitter.z,
+            floorLen = rCanvas.height - floorPos;
+            
+        let floorGrad = rctx.createLinearGradient(spacing * curr, floorPos - floorLen, spacing * curr, floorPos + floorLen);
+        floorGrad.addColorStop(0.5, rctx.fillStyle);
+        floorGrad.addColorStop(1, "white");
+        
+        let ceilLen = (rCanvas.height / 2 - rectLen / 2) + this.emitter.z,
+            ceilPos = 0;
+
+        // Ceiling
+        let ceilGrad = rctx.createLinearGradient(spacing * curr, ceilPos + ceilLen, spacing * curr, ceilPos);
+        ceilGrad.addColorStop(0, rctx.fillStyle);
+        ceilGrad.addColorStop(1, "white");
+        
+        
+        rctx.fillStyle = floorGrad
+        rctx.fillRect(spacing * curr, floorPos, spacing, floorLen);
+        rctx.fillStyle = ceilGrad;
+        rctx.fillRect(spacing * curr, ceilPos, spacing, ceilLen);
     }
 }
 
 let player = new Player(cCanvas.width / 2, cCanvas.height / 2, 15),
-    walls = [
+    presetWalls = [
         new Bound(50, 50, 50, 350), 
         new Bound(50, 350, 700, 350), 
         new Bound(700, 350, 700, 50), 
         new Bound(50, 50, 700, 50), 
     ];
 
-let mouse = {x: 0, y: 0,},
-    keys = {
-        up: {pressed: false},
-        left: {pressed: false},
-        down: {pressed: false},
-        right: {pressed: false},
-    };
+let mouse = {
+    x: 0, 
+    y: 0,
+    hold: false,
+    clicked: false,
+};
+
+let keys = {
+    up: {pressed: false},
+    left: {pressed: false},
+    down: {pressed: false},
+    right: {pressed: false},
+};
 
 
 window.addEventListener("keydown", (e) => {
     switch (e.key) {
-        case "ArrowUp":
+        case "w":
             keys.up.pressed = true;
             break;
-        case "ArrowLeft":
+        case "a":
             keys.left.pressed = true;
             break;
-        case "ArrowDown":
+        case "s":
             keys.down.pressed = true;
             break;
-        case "ArrowRight":
+        case "d":
             keys.right.pressed = true;
             break;
     }
@@ -258,33 +346,50 @@ window.addEventListener("keydown", (e) => {
 
 window.addEventListener("keyup", (e) => {
     switch (e.key) {
-        case "ArrowUp":
+        case "w":
             keys.up.pressed = false;
             break;
-        case "ArrowLeft":
+        case "a":
             keys.left.pressed = false;
             break;
-        case "ArrowDown":
+        case "s":
             keys.down.pressed = false;
             break;
-        case "ArrowRight":
+        case "d":
             keys.right.pressed = false;
             break;
     }
 });
 
+window.addEventListener("mousemove", (e) => {
+    mouse.x = (e.clientX - boundingRect.left) * scale;
+    mouse.y = (e.clientY - boundingRect.top) * scale;
+})
 
+window.addEventListener("mousedown", (e) => {
+    mouse.hold = true;
+})
+
+window.addEventListener("mouseup", (e) => {
+    mouse.hold = false;
+})
+
+let walls = [];
 function main() {
     requestAnimationFrame(main);
 
     cctx.clearRect(0, 0, cCanvas.width, cCanvas.height);
     rctx.clearRect(0, 0, cCanvas.width, cCanvas.height);
 
-    walls.forEach((wall) => wall.draw());
-    player.draw();
-    player.movement();
-    let rays = new Rays(player, fov=90, num=50, sight=500);
+    
+    for (let wall of walls) wall.draw();
+    let rays = new Rays(player, fov=90, num=500, sight=1000);
     rays.draw(bounds=walls);
+    player.movement();
+    player.draw();
+    
+    let potentialBound = Bound.create();
+    if (potentialBound) walls.push(potentialBound);
 }
 
 main();
